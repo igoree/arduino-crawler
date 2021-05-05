5d7bf55108702356c1adbafc71945a78c65e6601
Igor Eroshkin <eroshkin.igor.ev@gmail.com>
2021-05-05 11:19:38 +0300

    Coroutines

--- crawler-main/CoroutineScheduler.cpp
+++ crawler-main/CoroutineScheduler.cpp
@@ -1,3 +1,35 @@
 #include "CoroutineScheduler.h"
 
+#define EMPTY_ARRAY_INDEX UINT8_C(255)
 
+Coroutine::Coroutine(uint8_t stackSize)
+{
+	_stackSize = stackSize;
+	_stack = new CoroutineFuncState[_stackSize];
+	_currentFuncIndex = EMPTY_ARRAY_INDEX;
+}
+
+Coroutine::~Coroutine()
+{
+	delete[] _stack;
+}
+
+CoroutineExecutionContext::CoroutineExecutionContext(Coroutine* coroutine, CoroutineFuncState* funcState)
+{
+	_coroutine = coroutine;
+	_funcState = funcState;
+}
+
+CoroutineExecutionContext::~CoroutineExecutionContext()
+{
+}
+
+CoroutineStep CoroutineExecutionContext::getCurrentStep()
+{
+	return _funcState->step;
+}
+
+void* CoroutineExecutionContext::getData()
+{
+	return _funcState->data;
+}
--- crawler-main/CoroutineScheduler.h
+++ crawler-main/CoroutineScheduler.h
@@ -1,17 +1,80 @@
 #ifndef _COROUTINESCHEDULER_h
 #define _COROUTINESCHEDULER_h
 
+#include "stdint.h"
+
 class CoroutineExecutionContext;
 
-typedef void (*AsyncFuncPointer)(CoroutineExecutionContext* context);
+typedef uint8_t CoroutineStep;
 
-class CoroutineExecutionContext {
+typedef CoroutineStep(*AsyncFuncPointer)(CoroutineExecutionContext* context);
+
+struct CoroutineTask 
+{
+	AsyncFuncPointer func;
+	void* data;
+};
+
+struct CoroutineFuncState
+{
+	CoroutineTask task;
+	uint8_t step;
+	unsigned long executeAfterMillis;
+};
+
+class Coroutine
+{
+private:
+	CoroutineFuncState* _stack;
+	uint8_t _stackSize;
+	uint8_t _currentFuncIndex;
 public:
-	uint8_t getStep();
-	void nextStep(uint8_t step);
-	void nextStep();
-	void delay(unsigned long ms);
+	Coroutine(uint8_t stackSize);
+	~Coroutine();
+
+	void start(CoroutineTask task);
+	void switchTo(CoroutineTask task);
+
+	void continueExecution();
+};
+
+class CoroutineExecutionContext
+{
+private:
+	Coroutine* _coroutine;
+	CoroutineFuncState* _funcState;
+public:
+	CoroutineExecutionContext(Coroutine* coroutine, CoroutineFuncState* funcState);
+	~CoroutineExecutionContext();
+
+	CoroutineStep getCurrentStep();
+	void* getData();
+
+	CoroutineStep repeat();
+	CoroutineStep delayThenRepeat(unsigned long delayMs);
+	CoroutineStep executeThenRepeat(AsyncFuncPointer asyncFunc, void* data = NULL, unsigned long startDelayMs = 0);
+
+	CoroutineStep next();
+	CoroutineStep delayThenNext(unsigned long delayMs);
+	CoroutineStep executeThenNext(AsyncFuncPointer asyncFunc, void* data = NULL, unsigned long startDelayMs = 0);
+
+	CoroutineStep goTo(CoroutineStep step);
+	CoroutineStep delayThenGoTo(CoroutineStep step, unsigned long delayMs);
+	CoroutineStep executeThenGoTo(CoroutineStep step, AsyncFuncPointer asyncFunc, void* data = NULL, unsigned long startDelayMs = 0);
+
+	CoroutineStep end();
+};
+
+class CoroutineScheduler
+{
+private:
+	Coroutine* _coroutines;
+	uint8_t _coroutineCount;
+public:
+	CoroutineScheduler(Coroutine* coroutines, uint8_t coroutineCount);
+	~CoroutineScheduler();
+
+	void continueCoroutines();
 };
 
 #endif
-
